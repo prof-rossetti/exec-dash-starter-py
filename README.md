422f7fee07b97e17a51b759f25e77c415692c91e
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-04 12:31:41 -0500

    Setup starter repo

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # exec-dash-starter-py
+
 A starter repository for the "Executive Dashboard" project.
+
+## Prerequisites
+
+  + Python 3.7
+
+## Installation
+
+Fork this project repository under your own control, then clone or download the resulting repository onto your computer. Then navigate there from the command line:
+
+```sh
+cd exec-dash-starter-py
+```
+
+## Usage
+
+Process monthly sales data to generate a report of business insights:
+
+```sh
+python monthly_sales.py
+```
+
+## [License](/LICENSE.md)
---
+++ monthly_sales.py
@@ -0,0 +1,23 @@
+# monthly_sales.py
+
+# TODO: import some modules and packages here
+
+# TODO: write some Python code here to produce the desired functionality...
+
+print("-----------------------)
+print("MONTH: March 2018")
+
+print("-----------------------)
+print("CRUNCHING THE DATA...")
+
+print("-----------------------)
+print("TOTAL MONTHLY SALES: $12,000.71")
+
+print("-----------------------)
+print("TOP SELLING PRODUCTS:")
+print("  1) Button-Down Shirt: $6,960.35")
+print("  2) Super Soft Hoodie: $1,875.00")
+print("  3) etc.")
+
+print("-----------------------)
+print("VISUALIZING THE DATA...")

91017d5e05a2823c113cdb8ae9a93bd580dd7ec5
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-05 13:57:47 -0500

    Fix syntax errors

--- monthly_sales.py
+++ monthly_sales.py
@@ -4,20 +4,20 @@
 
 # TODO: write some Python code here to produce the desired functionality...
 
-print("-----------------------)
+print("-----------------------")
 print("MONTH: March 2018")
 
-print("-----------------------)
+print("-----------------------")
 print("CRUNCHING THE DATA...")
 
-print("-----------------------)
+print("-----------------------")
 print("TOTAL MONTHLY SALES: $12,000.71")
 
-print("-----------------------)
+print("-----------------------")
 print("TOP SELLING PRODUCTS:")
 print("  1) Button-Down Shirt: $6,960.35")
 print("  2) Super Soft Hoodie: $1,875.00")
 print("  3) etc.")
 
-print("-----------------------)
+print("-----------------------")
 print("VISUALIZING THE DATA...")
